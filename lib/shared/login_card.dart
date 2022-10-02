8fcb480510f997a6c5a740df7e8b1e0e28716a7f
Luis Carlos Murillo <lcmurillor@est.utn.ac.cr>
2022-10-01 22:54:07 -0600

    Creación del login screen

---
+++ lib/shared/login_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginCard extends StatelessWidget {
+  const LoginCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      child: SizedBox(height: 100, width: 100),
+    );
+  }
+}
--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hotel_primavera_app/shared/shareds.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text('hola');
+    final size = MediaQuery.of(context).size;
+    return Row(
+      children: [
+        Container(
+          width: size.width * 0.50,
+          height: size.height,
+          color: const Color.fromRGBO(227, 246, 252, 1),
+          child: Center(
+            child: SvgPicture.asset('logo.svg',
+                semanticsLabel: 'Logo Hotel Primavera',
+                fit: BoxFit.cover,
+                width: 500),
+          ),
+        ),
+        SizedBox(
+          width: size.width * 0.50,
+          height: size.height,
+          child: const Center(child: LoginCard()),
+        )
+      ],
+    );
   }
 }
